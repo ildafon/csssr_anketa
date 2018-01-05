9ecfe940dcd3d61904298bb00766a36a5754fb4e
ildafon <ildafon@yandex.ru>
2018-01-05 07:17:59 +0300

    creation-date done

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,14 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
 
+
+document.querySelector('.field__input--textarea').addEventListener('keyup', function () {
+	this.style.height = '6em';
+	this.style.height = this.scrollHeight + 'px';
+});
+
 $(() => {
 	svg4everybody();
 });
+
+
